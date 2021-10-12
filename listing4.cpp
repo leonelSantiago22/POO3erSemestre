41e162ba9bfcbc3a8da0abf8f031199402d9693e
leonelSantiago22 <santiago.rosas.leonel@gmail.com>
2021-10-12 16:10:19 -0500

    Se termino el listing 4

--- listing4.cpp
+++ listing4.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 
 using namespace std;
+int b =1;
+int f(int);
+int h(int x = f(b));
 
 int main()
 {
-    
+    b =1; 
+    cout << b << endl;
+    {
+        int b =1;
+        cout <<b<<endl;
+        cout << h(b);
+    }
+    return 0;
+}
+
+int h(int z )
+{
+    cout <<"valor recibido: " <<z << endl;
+    return z*z;
+}
+int f (int y)
+{
+    return y;
 }
